2065115249f80387a64af58245a76bd5245fd876
ivan97LOL <ivan777iag@gmail.com>
2024-05-16 23:06:43 +0200

    falta testear

--- 23-24/P8/mcp_bt.cc
+++ 23-24/P8/mcp_bt.cc
@@ -3,11 +3,10 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include <limits>
 #include <map>
 using namespace std;
 
-const int INF = INTMAX_MAX;
+const int INF = numeric_limits<int>::max();
 int visit, explored, leaf, unfeasible, not_promising;
 
 void usage() {
@@ -39,6 +38,7 @@ bool inside_matrix(int i, int j, int n, int m) {
     return i >= 0 && i < n && j >= 0 && j < m;
 }
 
+//Función que devuelve el camino óptimo de manera iterativa
 int mcp_it_matrix(const vector<vector<int>> &map, vector<vector<int>> &iterative) {
     int n = map.size();
     int m = map[0].size();
@@ -60,11 +60,10 @@ int mcp_it_matrix(const vector<vector<int>> &map, vector<vector<int>> &iterative
     }
 
     return iterative[n-1][m-1];
-
 }
 
 //Función backtracking
-int mcp_bt(const vector<vector<int>> &maze, vector<vector<int>> &iterative, vector<vector<bool>> &visited, int x, int y){
+int mcp_bt(const vector<vector<int>> &maze, vector<vector<int>> &iterative, vector<vector<bool>> &visited, int x, int y, int &min_cost){
     enum Step {SE, E, S, N, NE, SW, W, NW};
     map<Step, tuple<int, int>> steps_inc;
 
@@ -77,9 +76,8 @@ int mcp_bt(const vector<vector<int>> &maze, vector<vector<int>> &iterative, vect
     steps_inc[W] = make_tuple(0,-1);
     steps_inc[NW] = make_tuple(-1,-1);
 
-    int min_cost = iterative[x][y];
-
     for(auto it = steps_inc.begin(); it != steps_inc.end(); it++){
+        visit++;
         int incx, incy;
 
         tie(incx, incy) = it->second;
@@ -88,32 +86,83 @@ int mcp_bt(const vector<vector<int>> &maze, vector<vector<int>> &iterative, vect
 
         //check factible
         if(inside_matrix(newx,newy,maze.size(), maze[0].size()) && !visited[newx][newy]){
-            visit++;
-            visited[newx][newy] = true;
-            int cost = maze[newx][newy] + mcp_bt(maze, iterative, visited, newx, newy);
+            
+            explored++;
 
-            //check promising
-            if(cost < min_cost){
-                explored++;
-                min_cost = cost;
-                iterative[x][y] = min_cost;
+            visited[newx][newy] = true;
+            
+            vector<int> min_rows;
+            vector<int> min_cols(maze[0].size(), INF);
+            vector<int> sumrows(maze.size());
+            vector<int> sumcols(maze[0].size());
+            
+            //encuentra el elemento mínimo de todas las filas
+            for (const auto& fila : maze) {
+                int minElement = *min_element(fila.begin(), fila.end());
+                min_rows.push_back(minElement);
             }
+
+            //encuentra el elemento mínimo de todas las columnas
+            for (const auto& fila : maze) {
+                for (int j = 0; j < fila.size(); j++) {
+                    min_cols[j] = std::min(min_cols[j], fila[j]);
+                }
+            }
+
+            int sumfilas = 0;
+            int sumcolumnas = 0;
+
+            //se almacena el sumatorio hasta la fila
+            for(const auto& element : min_rows){
+                sumfilas += element;
+                sumrows.push_back(sumfilas);
+            }
+
+            //se almacena el sumatorio hasta la columna
+            for(const auto& element : min_cols){
+                sumcolumnas += element;
+                sumcols.push_back(sumcolumnas);
+            }
+
+            int future_cost = max(sumrows[newx], sumcols[newy]);
+
+            if(future_cost > min_cost){
+                unfeasible++;
+            }
+            else{
+
+                int cost = iterative[newx][newy] + mcp_bt(maze, iterative, visited, newx, newy, min_cost) + future_cost;
+
+                //check promising
+                if(cost < min_cost){
+                    min_cost = cost;
+                    iterative[x][y] = cost;
+                }
+                else{
+                    not_promising++;
+                }
+            }
+            
             visited[newx][newy] = false;
-        }    
+        }
+        
+        //check leaf
+        if(x == maze.size() - 1 && y == maze[0].size() - 1){
+            leaf++;
+        }
     }
-    return iterative[x][y];
+
+    return min_cost;
 }
 
 //Función que muestra la salida según parámetros
 void output(bool p, bool p2D, vector<vector<int>> map, int r, int c){
 
-    
-    
     vector<vector<bool>> path(r, vector<bool>(c, false));
     vector<vector<int>> iterative(r, vector<int>(c, INF));
     int shortest_path_iterative = mcp_it_matrix(map, iterative);
 
-    int bt = mcp_bt(map, iterative, path, 0, 0);
+    int bt = mcp_bt(map, iterative, path, 0, 0, shortest_path_iterative);
 
     cout<<bt<<endl;
     cout<<visit<<" "<<explored<<" "<<leaf<<" "<<unfeasible<<" "<<not_promising<<endl;
